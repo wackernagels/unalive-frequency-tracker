0c1ae34c2034c01e685e57a9609440d058bfa2ad
Atticus Yang <ayang5@uchicago.edu>
2025-03-14 04:50:30 -0500

    finished vis

--- vis.py
+++ vis.py
@@ -1,6 +1,6 @@
 import plotly.express as px
 import plotly.graph_objects as go
-import plotly as p
+from plotly.subplots import make_subplots
 import pandas as pd
 
 #reading our data in
@@ -8,45 +8,93 @@ df = pd.read_csv('cleaned_data.csv')
 df['date']= pd.to_datetime(df["date"])
 
 #initializing the empty graph
-postvis= go.Figure()
+postvis= make_subplots(rows=2,
+                       cols=1,
+                       vertical_spacing= 0.05,
+                       shared_xaxes=True)
 
-#adding in scatterplot for unalive
+#adding in line plot format for unalive
 postvis.add_trace(
     go.Scatter(x=df['date'],
                y=df['unalive'],
                name='unalive', 
-               line_shape='spline')
+               line_shape='spline'),
+    row=1, col=1,
 )
 #scatterplot for kill
 postvis.add_trace(
     go.Scatter(x=df['date'],
                y=df['kill'],
                name='kill',
-               line_shape='spline')
+               line_shape='spline'),
+    row=1, col=1,
 )
 #murder
 postvis.add_trace(
     go.Scatter(x=df['date'],
                y=df['murder'],
                name='murder',
-               line_shape='spline')
+               line_shape='spline'),
+    row=1, col=1,
 )
 #suicide
 postvis.add_trace(
     go.Scatter(x=df['date'],
                y=df['suicide'],
                name='suicide',
-               line_shape='spline')
+               line_shape='spline'),
+    row=1, col=1,
 )
 
-#updating the layout
+#adding hover for the graph
+postvis.update_traces(mode="lines", hovertemplate=None)
+postvis.update_layout(hovermode="x unified")
+
+#adding bar chart vis
+postvis.add_trace(
+    go.Bar(
+        x=df['date'],
+        y=df['unalive'],
+        name='unalive',
+        marker_color='#636EFA'),
+    row=2, col=1,
+)
+postvis.add_trace(
+    go.Bar(
+        x=df['date'],
+        y=df['kill'],
+        name='kill',
+        marker_color='#EF553B'),
+    row=2, col=1,
+)
+postvis.add_trace(
+    go.Bar(
+        x=df['date'],
+        y=df['murder'],
+        name='murder',
+        marker_color='#00CC96'),
+    row=2, col=1,
+)
+postvis.add_trace(
+    go.Bar(
+        x=df['date'],
+        y=df['suicide'],
+        name='suicide',
+        marker_color='#AB63FA'),
+    row=2, col=1,
+)
+
+postvis.update_layout(barmode='stack')
+
 postvis.update_layout(
+    plot_bgcolor='#f4f4f4',
+    paper_bgcolor='white',
     #adding title to vis
-    title= 'Number of Posts on r/offmychest Containing Certain Words, 2019-2024',
+    title= 'Number of Posts on r/offmychest Containing Killing-Related Words, 2019-2024',
     #setting range of axis
     yaxis_range=[0, 70],
     #adding axis label and slider for specifying time range
-    xaxis=dict(
+    xaxis2=dict(
         title=dict(
             text="Time"),
         #adding slider for dates
@@ -57,49 +105,20 @@ postvis.update_layout(
     ),
     #labeling axis
     yaxis=dict(
+        gridcolor='lightgrey',
         title=dict(
             text="Number of Posts"
         )
     ),
-    updatemenus=[
-        dict(
-            type="buttons",
-            buttons=[
-                dict(label="2019",
-                     method="relayout",
-                     args=["xaxis", {'range':('2019-01-01', '2019-12-31'),
-                                    'rangeslider':dict(visible=True),}]),
-                dict(label="2020",
-                     method="relayout",
-                     args=["xaxis", {'range':('2020-01-01', '2020-12-31'),
-                                    'rangeslider':dict(visible=True),}]),
-                dict(label="2021",
-                     method="relayout",
-                     args=["xaxis", {'range':('2021-01-01', '2021-12-31'),
-                                    'rangeslider':dict(visible=True),}]),
-                dict(label="2022",
-                     method="relayout",
-                     args=["xaxis", {'range':('2022-01-01', '2022-12-31'),
-                                    'rangeslider':dict(visible=True),}]),
-                dict(label="2023",
-                     method="relayout",
-                     args=["xaxis", {'range':('2023-01-01', '2023-12-31'),
-                                    'rangeslider':dict(visible=True),}]),
-                dict(label="2024",
-                     method="relayout",
-                     args=["xaxis", {'range':('2024-01-01', '2024-12-31'),
-                                    'rangeslider':dict(visible=True),}])]
-        )],
+    yaxis2=dict(
+        gridcolor='lightgrey',
+        title=dict(
+            text="Number of Posts"
+        )
+    ),
 )
 
-#adding buttons to set slider for certain years
-postvis.update_layout(
-    
-)
 
-#adding hover for the graph
-postvis.update_traces(mode="lines", hovertemplate=None)
-postvis.update_layout(hovermode="x unified")
 
 #-----------END updating postvis visuals---------
 
